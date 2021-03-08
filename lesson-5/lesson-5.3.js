9cc965c6746dee73f8f928e9a714bbedb15d145e
Solyankina <solyankina.anastasia@gmail.com>
2021-03-08 14:55:16 +0300

    дз к уроку 5

---
+++ lesson-5/lesson-5.3.js
@@ -0,0 +1,56 @@
+'use strict';
+const cart = {
+	products: [
+	    {	name: 'avocado',
+	    	price: 200,
+	    	count: 2,
+		},
+
+	    {	name: 'apple',
+	    	price: 30,
+	    	count: 10,
+		},
+
+	    {	name: 'lemon',
+			price: 40,
+			count: 10,
+		},
+
+	    {	name: 'mango',
+			price: 300,
+			count: 1,
+		}
+	],
+
+	totalPrice() {
+		let sum = 0;
+		for (var i = 0; i < this.products.length; i++) {
+			let product = this.products[i];
+			sum += product.price * product.count;
+		}
+		return sum;
+	},
+
+	totalCount() {
+		let count = 0;
+		for (var i = 0; i < this.products.length; i++) {
+			count += this.products[i].count;
+		}
+		return count;
+	}
+}
+
+function renderCart(target) {
+	let divElement = document.createElement('div');
+	let message = 'Корзина пуста';
+
+	if (cart.length != 0) {
+		message = 'В корзине: ' + cart.totalCount() + ' товаров на сумму ' + cart.totalPrice() + ' рублей';
+	}
+
+	divElement.innerHTML = message;
+	target.appendChild(divElement); 
+}
+
+
+renderCart(document.body);
